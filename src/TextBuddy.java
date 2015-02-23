1997cdc5d3e45f6126f7d836abf3137949fdc698
Sean-Tay <sean@u.nus.edu>
2015-02-23 23:15:27 +0800

    Changed the entire implementation of TextBuddyHelper to reduce code length (by about half). Used fileContents to hold the state of the loaded file instead of directly reading and writing to the file over and over again.

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -13,7 +13,6 @@
 public class TextBuddy {
 	public static void main(String args[])
 	{
-		new TextBuddyHelper();
-		TextBuddyHelper.start(args);
+		new TextBuddyHelper(args[0]);
 	}
 }
--- src/TextBuddyHelper.java
+++ src/TextBuddyHelper.java
@@ -1,5 +1,5 @@
 /*
- *  TextBuddyHelper [Build Version 0.0]
+ *  TextBuddyHelper [Build Version 1.0]
  *  Tay Siang Meng Sean
  */
 
@@ -15,111 +15,91 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-class TextBuddyHelper{
+public class TextBuddyHelper {
 	
 	static String filePath = System.getProperty("user.dir");
 	
 	//Constants
 	static final String GEN_ERROR = "Something went wrong!";
 	
-	//File IO variables
 	private static File file;
-	private static File tempFile;
-	
-	private static FileWriter fileWriter;
-	private static BufferedWriter writer;
-	
-	private static FileWriter tempWriter;
-	private static BufferedWriter tWriter;
-	
-	private static FileReader fileReader;
-	private static BufferedReader reader;
+	private static List<String> fileContents;
 	
 	//First-Tier Operations
 	
-	public TextBuddyHelper() { 
+	public TextBuddyHelper(String fileName) {
 		
 		/**
-		 *  Constructor to initialize IO elements
-		 */
-
-		file = null;
-		tempFile = null;
-		
-		fileWriter = null;
-		writer = null;
-		
-		tempWriter = null;
-		tWriter = null;
-		
-		fileReader = null;
-		reader = null;
-		
-	}
-	
-	public static void start(String[] args) { 
-
-		/**
-		 * Program operation starts here
-		 */
-
-		preErrorChecks(args); 
-		
-	    createFileProcess(args); 
-	    
-		printWelcomeMessage();
-	    
-	    displayMenu(); 
-		 
-	}
-	
-	private static void preErrorChecks(String args[]) { 
-
-		/**
-		 *  Perform validity checks on given arguments
+		 * Initialize elements
 		 */
 		
-		if (args.length <= 0) {
-	    	System.out.println("No command-line arguments given. Closing program.");
-	        System.exit(0);
-		}
-	}
-	
-	private static void createFileProcess(String args[]) { 
-
-		/**
-		 * To start the file creation process
-		 */
+		file = new File(fileName);
+		fileContents = new ArrayList<String>();
 		
-		file = new File(args[0]); 
+		checkFileExists();
 		
-    	//checkIfFileExists();
-    	
 		createTheFile();
+		
+		printWelcomeMessage();
+		
+		displayMenu();
 	}
 
-	private static void checkIfFileExists() { 
-
+	private void checkFileExists() {
+		
 		/**
-		 * Check if the file exists
+		 * Checks if the file exists. If file exists, read in contents of file and store in fileContents list. Else, print not found message, and leave fileContents to be empty. 
 		 */
 		
 		if (!file.exists()) {
-			printNotFileFoundMsg(file);
-    	}
+			
+			System.out.println(file.toString() + " does not exist in root directory: " + filePath + ". Creating the file now.");
+		}
+		else {
+			
+			fileContents = fillList(fileContents);
+		}
 	}
 	
-	private static void printNotFileFoundMsg(File file) { 
-
+	private static List<String> fillList(List<String> list) {
+		
 		/**
-		 *  Function to print out a message stating where the newly created file will be
+		 * Fills a given list with file contents, and returns the list
 		 */
 		
-		System.out.print("No file found. Creating a file called \"" + file.toString() + "\" at: ");
-		System.out.println(filePath + ".");
-		System.out.println();
+		try {
+			
+			FileReader fileReader = new FileReader(file);
+			BufferedReader reader = new BufferedReader(fileReader);
+			
+			String line = reader.readLine();
+			
+			if (line == null) {
+				
+				System.out.println(file.toString() + " is empty.");
+			}
+			
+			int lineNum = 1;
+			
+			while (line != null) {
+				
+				list.add(line);
+				line = reader.readLine();
+				lineNum++;
+			}
+			
+			reader.close();
+			fileReader.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Error with reader.");
+		} catch (IOException e) {
+			System.out.println("Error with loading content from file.");
+		}
+		
+		return list;
 	}
-
+	
 	private static void createTheFile() { 
 
 		/**
@@ -127,7 +107,7 @@ class TextBuddyHelper{
 		 */
 		
 		try {	
-			fileWriter = new FileWriter(file, file.exists());
+			FileWriter fileWriter = new FileWriter(file, file.exists());
 			fileWriter.close();
 		} catch (IOException e) {
 			System.out.println("Error with Writer. Please restart the application.");
@@ -145,8 +125,8 @@ class TextBuddyHelper{
 		System.out.print("\"" + file.toString() + "\" ");
 		System.out.println("is ready for use.");
 	}
-	
-	//Second Tier Operations
+
+	//Second-Tier Operations
 	
 	private static void displayMenu() { 
 
@@ -230,597 +210,198 @@ class TextBuddyHelper{
 					sc.nextLine();
 					break;
 			}
-				
+			
+			writeList(fileContents);
+	
 		}
 		
 		sc.close();
-		resetIO();
 		
 	}
-	
-	//canAddText Operations
-	
-	private static boolean canAddText(String toAdd) { 
 
-		/**
-		 * Function to call when adding one line of text to document
-		 */
+	private static void writeList(List<String> list){
 		
-		toAdd = toAdd.substring(1, toAdd.length());
+		/**
+		 * Writes a given list of Strings to the file
+		 */
 		
 		try {
-			addTextInit();
+			FileWriter fileWriter = new FileWriter(file, false);
+			BufferedWriter writer = new BufferedWriter(fileWriter);
 			
-			addingTheText(toAdd);
-			
-			return true;
-			
-		} catch (IOException e) {
-			
-		}
-		return false;
-	}
-
-	private static void addTextInit() throws IOException { 
-
-		/**
-		 * Initializes canAddText variables
-		 */
-		
-		resetIO();
-		
-		fileWriter = new FileWriter(file, true);
-		writer = new BufferedWriter(fileWriter);
-	}
-		
-	private static void addingTheText(String toAdd) throws IOException { 
-
-		/**
-		 * The code that actually adds the line to the file
-		 */
-		
-		writer.write(toAdd);
-		writer.newLine();
-		writer.flush(); //Needed to reset stream
-		
-		System.out.println("added to " + file.toString() + ": " + "\"" + toAdd + "\"");
-		
-		resetIO();
-	}
-	
-	//canDisplayText Operations
-	
-	private static boolean canDisplayText() { 
-
-		/**
-		 * Function to call for displaying contents of loaded text file
-		 */
-		
-		displayTextInit();
-		
-		try {
-			
-			displayingTheText();
-
-			return true;
-			
-		} catch (IOException e) {
-			
-			return false;
-		}		
-	}
-
-	private static void displayTextInit() {
-		
-		/**
-		 * Initializes IO variables for canDisplayText()
-		 */
-		
-		resetIO();
-		
-		try {
-			fileReader = new FileReader(file);
-		} catch (FileNotFoundException e) {
-			System.out.println("Error with Reader.");
-		}
-		reader = new BufferedReader(fileReader);
-	}
-	
-	private static void displayingTheText() throws IOException {
-		
-		/**
-		 * The code that actually displays the text to the user
-		 */
-		
-		String line = reader.readLine();
-		
-		if (line == null) {
-			
-			System.out.println(file.toString() + " is empty");
-		}
-		
-		int lineNum = 1;
-		
-		while (line != null) {
-			
-			System.out.println(lineNum + ". " + line);
-			line = reader.readLine();
-			lineNum++;
-		}
-		
-		resetIO();
-	}
-	
-	//canDeleteText Operations
-	
-	private static boolean canDeleteText(int givenLineNum) { 
-
-		/**
-		 * Function to call when a line is to be 'deleted' from the text file 
-		 */
-		
-		String nameOfFile = file.toString();
-		String deletedLine = null;
-		String line = null;
-		
-		deleteTextInit();
-		
-		try {
-			
-			int currLineNum = 1;
-			boolean invalidLineNumGiven = true;
-
-			line = reader.readLine();
-			
-			while (line != null) {
-				
-				if (currLineNum == givenLineNum) {
-					
-					invalidLineNumGiven = false;
-					deletedLine = line;
-					line = reader.readLine();
-					currLineNum++;
-				}
-
-				if (line != null) {
-					
-					tWriter.write(line);
-					tWriter.newLine();
-					line = reader.readLine();
-				}
-				
-				currLineNum++;
-				
+			for (int index=0; index<list.size(); index++) {
+				writer.write(list.get(index) + "\n");
 			}
 			
-			deleteTextComplete(nameOfFile, deletedLine, invalidLineNumGiven);
-			
-			return true;
-			
-		}
-		catch (IOException e) {
-			
-		}
-		catch(NullPointerException e2) {
-			
-			System.out.println("Unable to continue reading. Line number given may have exceeded current number of lines in text file.");
-		}
-		return false;
-	}
-
-	private static void deleteTextInit() { 
-		
-		resetIO();
-		
-		/** 
-		 * Initializes canDeleteText() IO variables
-		 */
-		
-		try {
-			fileReader = new FileReader(file);
-		} catch (FileNotFoundException e) {
-			System.out.println("Error with Reader.");
-		}
-		reader = new BufferedReader(fileReader);
-		
-		tempFile = new File("tempfile.txt");
-		try {
-			tempWriter = new FileWriter(tempFile);
-		} catch (IOException e) {
-			System.out.println("Unable to create temporary file. Recommend restarting program.");
-		}
-		tWriter = new BufferedWriter(tempWriter);
-	}
-	
-	private static void deleteTextComplete(String nameOfFile, String deletedLine, boolean invalidLineNumGiven) throws IOException {
-		
-		/**
-		 * Overwrites the old file with the new file that does not contain the deleted line
-		 */
-		
-		resetIO();
-		
-		if (!invalidLineNumGiven)
-		{
-			//Given line number was valid. Proceed to replace original file with edited tempFile
-			
-			file.delete();
-			
-			boolean success = tempFile.renameTo(file); //Can only be done AFTER the writer closes
-			
-			file = new File(nameOfFile);
-			
-			System.out.println("deleted from " + file.toString() + " : " + "\"" + deletedLine + "\"");
-		}
-		else
-		{
-			System.out.println("Unable to delete line that does not exist.");
-		}
-		
-		tempFile.delete();
-	}
-	
-	//canClearText Operations
-	
-	private static boolean canClearText() { 
-
-		/**
-		 * Function called when deleting all content from loaded text file
-		 */
-		
-		if (!canOverwrite()) {
-			return false;
-		}
-		
-		System.out.println("all content deleted from " + file.toString());
-		return true;
-		
-	}
-
-	private static boolean canOverwrite() {
-		
-		/**
-		 * Overwrites main file
-		 */
-		
-		resetIO();
-		
-		
-		try {
-			fileWriter = new FileWriter (file);
+			writer.close();
 			fileWriter.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
 		} catch (IOException e) {
-			return false;
+			System.out.println("Error with writing content to file.");
+		}
+	}
+
+	//canAddText()
+	
+	private static boolean canAddText(String toAdd) {
+		
+		/**
+		 * Appends a given line of text to fileContents
+		 */
+		
+		if (checkValidString(toAdd)) {
+			
+			toAdd = toAdd.substring(1, toAdd.length());
+			fileContents.add(toAdd);
+			System.out.println("Added: " + toAdd);
+			return true;
 		}
 		
+		return false;
+		
+	}
+	
+	//canDisplayText()
+	
+	private static boolean canDisplayText() {
+		
+		/**
+		 * Prints fileContents.
+		 */
+		
+		printList(fileContents, true, fileContents);
 		return true;
 	}
+	
+	//canDeleteText()
+	
+	private static boolean canDeleteText(int lineNum) {
 		
-	//canSortText Operations
+		/**
+		 * Deletes a line from fileContents
+		 * Assumes user does not take into account that the starting index is 0 and not 1
+		 */
+		
+		try {
+			
+			
+			String removedLine = fileContents.remove(lineNum - 1);
+			System.out.println("Deleted: " + removedLine);
+			
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Unable to remove non-existent line. Please input a number from: 1 to " + fileContents.size() + ".");
+			return false;
+		}
+		return true;
+	}
+
+	//canClearText()
+	
+	private static boolean canClearText() {
+		
+		/**
+		 * Deletes all content from fileContents
+		 */
+		
+		fileContents.clear();
+		System.out.println("All content cleared.");
+		return fileContents.isEmpty();
+	}
+	
+	//canSortText()
 	
 	private static boolean canSortText() {
 		
-		try {
+		/**
+		 * Sorts fileContents
+		 */
+		
+		Collections.sort(fileContents);
+		System.out.println("Items sorted.");
+		return true;
+	}
+
+	//canSearchText()
+	
+	private static boolean canSearchText(String searchItem) {
+		
+		/**
+		 * Given a searchItem, checks if it exists in fileContents.
+		 */
+		
+		List<String> results = new ArrayList<String>();
+		searchItem = searchItem .substring(1, searchItem.length());
+		
+		for (int i=0; i<fileContents.size(); i++) {
 			
-			List<String> fileContents = sortTextInit();
-			fileContents = sortingTheText(fileContents);
-			sortTextComplete(fileContents);
-			
-		} catch (IOException e) {
+			if (fileContents.get(i).contains(searchItem)) {
+				results.add(fileContents.get(i));
+			}
+		}
+		
+		printList(results, false, fileContents);
+		return true;
+	}
+	
+	//Additional Functions
+	private static boolean checkValidString(String toCheck) {
+		
+		/**
+		 * Check if given String is valid.
+		 */
+		
+		if (toCheck.isEmpty()) {
 			return false;
 		}
 		
 		return true;
 	}
-	
-	private static List<String> sortTextInit() throws IOException{
 		
-		/**
-		 * Initializes the variables used in canSortText()
-		 */
-		
-		List<String> fileContents = new ArrayList<String>();
-		fileContents = fillList(fileContents);
-		return fileContents;
-	}
-	
-	private static List<String> sortingTheText(List<String> fileContents) throws IOException {
-		
-		/**
-		 * Code that actually sorts the text
-		 */
-		
-		Collections.sort(fileContents);
-		
-		return fileContents;
-	}
-	
-	private static void sortTextComplete(List<String> fileContents) {
-		
-		/**
-		 * Clears and writes the given list to the file
-		 */
-		
-		
-		try {
-			canClearText();
-			writeList(fileContents);
-		} catch (IOException e) {
-			
-		}
-		
-	}
-	
-	//canSearchText Operations
-	private static boolean canSearchText(String searchItem) {
-		
-		/**
-		 * Searches for a given string within the file and prints the corresponding lines of text in the file that contains the searchItem
-		 * Assumes user does not search for a particular Line Number
-		 */
-		
-		System.out.println("Search Item Given:" + searchItem);
-		
-		if (checkValidSearchString(searchItem)) {
-			
-			List<String> searchResults = searchTextInit();
-			
-			try {
-				
-				searchResults = searchingTheText(searchResults, searchItem);
-				searchTextComplete(searchResults);
-				
-			} catch (IOException e) {
-				System.out.println("Unable to search text.");
-			}
-			
-			return true;
-			
-		}
-		
-		return false;
-	}
-	
-	private static boolean checkValidSearchString(String searchItem) {
-		
-		/**
-		 * Actual checks for checking if searchItem given is valid
-		 */
-		
-		if (searchItem.length()==0) {
-			
-			System.out.println("Please enter a valid non-empty search item.");
-		}
-		
-		return true;
-	}
-	
-	private static List<String> searchTextInit() {
-		
-		/**
-		 * Initializes canSearchText() IO variables
-		 */
-
-		resetIO();
-		
-		try {
-			fileReader = new FileReader(file);
-		} catch (FileNotFoundException e) {
-			System.out.println("Error with reader.");
-		}
-		reader = new BufferedReader(fileReader);
-		
-		List<String> searchResults = new ArrayList<String>();
-		
-		return searchResults;
-	}
-	
-	private static List<String> searchingTheText(List<String> searchResults, String searchItem) throws IOException {
-		
-		/**
-		 * Code that actually does the searching
-		 */
-		
-		searchItem = searchItem.substring(1, searchItem.length());
-		
-		String line = reader.readLine();
-		
-		if (line == null) {
-			
-			System.out.println(file.toString() + " is empty");
-		}
-		
-		int lineNum = 1;
-		
-		while (line != null) {
-			
-			if (line.contains(searchItem)) {
-				
-				searchResults.add(lineNum + ". " + line);
-			}
-
-			line = reader.readLine();
-			lineNum++;
-		}
-		
-		return searchResults;
-		
-	}
-	
-	private static void searchTextComplete(List<String> searchResults) {
-		
-		/**
-		 * Prints out the list of search items found
-		 */
-
-		resetIO();
-		
-		if (searchResults.size() != 0) {
-			printList(searchResults);
-		}
-		else {
-			System.out.println("No items match or contain the search item.");
-		}
-	}
-	
-	//Additional Functions
-	
-	private static List<String> fillList(List<String> list) throws IOException {
-		/**
-		 * Fills a given list with file contents, and returns the list
-		 */
-		
-		resetIO();
-		
-		try {
-			fileReader = new FileReader(file);
-		} catch (FileNotFoundException e) {
-			System.out.println("Error with reader");
-		}
-		reader = new BufferedReader(fileReader);
-		
-		String line = reader.readLine();
-		
-		if (line == null) {
-			
-			System.out.println(file.toString() + " is empty");
-		}
-		
-		int lineNum = 1;
-		
-		while (line != null) {
-			
-			list.add(line);
-			line = reader.readLine();
-			lineNum++;
-		}
-		
-		resetIO();
-		
-		return list;
-	}
-	
-	private static void writeList(List<String> list) throws IOException{
-		
-		/**
-		 * Appends a given list of Strings to the file
-		 */
-		
-		resetIO();
-		
-		try {
-			fileWriter = new FileWriter(file, true);
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-		}
-		writer = new BufferedWriter(fileWriter);
-		
-		for (int index=0; index<list.size(); index++) {
-			writer.write(list.get(index) + "\n");
-		}
-		
-		resetIO();
-		
-	}
-	
-	private static void printList(List<String> list)
+	private static void printList(List<String> list, boolean defaultBehavior, List<String> secondList)
 	{
 		/**
-		 * Given a list, prints all contents out
+		 * Given a list, prints all contents out, behavior changes according to second argument
 		 */
 		
-		System.out.println("List contents: ");
-		
-		for (int index=0; index<list.size(); index++) {
+		if (defaultBehavior) {
 			
-			System.out.println(list.get(index) + " ");
+			/**
+			 * Prints accompanying index as per normal
+			 */
+			
+			if (!list.isEmpty()) {
+				System.out.println("All related content: ");
+				
+				for (int index=0; index<list.size(); index++) {
+					
+					System.out.println((index+1) + ". " + list.get(index));
+				}
+			}
+			else
+			{
+				System.out.println("No content to display.");
+			}
 		}
-		
+		else {
+			
+			/**
+			 * Prints accompanying index in relation to secondList
+			 */
+			
+			System.out.println("Results: ");
+			
+			for (int index=0; index<list.size(); index++) {
+				
+				System.out.println((secondList.indexOf(list.get(index))+1) + ". " + list.get(index));
+			}
+			
+			
+		}
+			
 	}
 	
-	private static void resetIO() {
-		
-		/**
-		 * To be called to force reset all IO handlers, except file and tempFile
-		 * Each try-catch has to be separated to be sure that all handlers are reset and not just some of them
-		 */
-		
-		try {
-			reader.close();
-		} 
-		catch (IOException e) {
-			
-		}	
-		catch (NullPointerException e2) {
-			
-		}
-		
-		try {
-			fileReader.close();
-		} 
-		catch (IOException e) {
-			
-		}
-		catch (NullPointerException e2)
-		{
-			
-		}
-		
-		try {
-			writer.close();
-		} 
-		catch (IOException e) {
-			
-		}
-		catch (NullPointerException e2)
-		{
-			
-		}
-		
-		try {
-			fileWriter.close();
-		} 
-		catch (IOException e) {
-			
-		}
-		catch (NullPointerException e2)
-		{
-			
-		}
-		
-		try {
-			tWriter.close();
-		} 
-		catch (IOException e) {
-			
-		}
-		catch (NullPointerException e2)
-		{
-			
-		}
-		
-		try {
-			tempWriter.close();
-		} 
-		catch (IOException e) {
-			
-		}
-		catch (NullPointerException e2)
-		{
-			
-		}
-		
-		reader = null;
-		fileReader = null;
-		
-		writer = null;
-		fileWriter = null;
-		
-		tWriter = null;
-		tempWriter = null;
-		
-		System.gc();
-	}
 }
 
 
