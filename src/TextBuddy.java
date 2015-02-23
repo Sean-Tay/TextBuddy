baed16cd350f153f4ebfc3341f85a2a55bf467ca
Sean-Tay <sean@u.nus.edu>
2015-02-23 14:43:04 +0800

    Testing new configuration on another laptop.

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -13,6 +13,8 @@
 public class TextBuddy {
 	public static void main(String args[])
 	{
+		//Testing new configuration on another laptop
+			
 		new TextBuddyHelper();
 		TextBuddyHelper.start(args);
 	}
