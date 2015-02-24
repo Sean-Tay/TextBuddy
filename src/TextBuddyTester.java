52211398ef83a1000ebce7ce59b946b9cf6106f4
Sean-Tay <sean@u.nus.edu>
2015-02-24 17:01:00 +0800

    Tester file for TextBuddy has been created.

---
+++ src/TextBuddyTester.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TextBuddyTester {
+
+	@Test
+	public void testTextBuddyHelper() {
+		fail("Not yet implemented");
+	}
+
+}
