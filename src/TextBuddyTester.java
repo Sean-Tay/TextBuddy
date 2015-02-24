537c648f327ac89b22d18303dd2294ad508975b5
Sean-Tay <sean@u.nus.edu>
2015-02-25 00:03:25 +0800

    Made the basic Test cases for CE1.

--- src/TextBuddyTester.java
+++ src/TextBuddyTester.java
@@ -1,12 +1,15 @@
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 import org.junit.Test;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
 public class TextBuddyTester {
 	
-	static TextBuddyHelper testInstance = null;
+	static TextBuddyHelper testInstance = new TextBuddyHelper ("mytestfile.txt", true);
 	
 	static String ADD_COMMAND = "add";
 	static String DISPLAY_COMMAND = "display";
@@ -24,10 +27,14 @@ public class TextBuddyTester {
 	static String ITEM7 = "Crimson Jazz";
 	
 	@Before
-	//Also tests Add function incidentally.
+	//Adds a single empty line first
 	public void setUp() {
-		
-		TextBuddyHelper testInstance = new TextBuddyHelper ("mytestfile.txt", true);
+		System.out.println();
+	}
+	
+	@Test
+	//Tests the Add function.
+	public void test1() {
 		
 		assertEquals("Added: " + ITEM1 + ". \n", testInstance.determineAndExecuteCommand(ADD_COMMAND, ITEM1));
 		assertEquals("Added: " + ITEM2 + ". \n", testInstance.determineAndExecuteCommand(ADD_COMMAND, ITEM2));
@@ -38,9 +45,72 @@ public class TextBuddyTester {
 
 	@Test 
 	//Tests the Display function.
-	public void testDisplay() {
+	public void test2() {
 
-			
+		assertEquals("All related content: " + "\n1. " + ITEM1 + "\n2. " + ITEM2 + "\n3. " + ITEM3 +"\n4. " + ITEM4 + "\n", testInstance.determineAndExecuteCommand(DISPLAY_COMMAND, null));
+	}
+	
+	@Test
+	//Tests the Delete function in a variety of settings (first line in file, middle line in file, last line in file, last item in the file)
+	public void test3() {
+		
+		assertEquals("Deleted: " + ITEM1 + ". \n", testInstance.determineAndExecuteCommand(DELETE_COMMAND, "1"));
+		assertEquals("Deleted: " + ITEM3 + ". \n", testInstance.determineAndExecuteCommand(DELETE_COMMAND, "2"));
+		assertEquals("Deleted: " + ITEM4 + ". \n", testInstance.determineAndExecuteCommand(DELETE_COMMAND, "2"));
+		
+		
+		assertEquals("All related content: " + "\n1. " + ITEM2 + "\n", testInstance.determineAndExecuteCommand(DISPLAY_COMMAND, null));
+		
+		
+		assertEquals("Deleted: " + ITEM2 + ". \n", testInstance.determineAndExecuteCommand(DELETE_COMMAND, "1"));
+		
+		
+		assertEquals("Added: " + ITEM1 + ". \n", testInstance.determineAndExecuteCommand(ADD_COMMAND, ITEM1));
+		assertEquals("Added: " + ITEM2 + ". \n", testInstance.determineAndExecuteCommand(ADD_COMMAND, ITEM2));
+		assertEquals("Added: " + ITEM3 + ". \n", testInstance.determineAndExecuteCommand(ADD_COMMAND, ITEM3));
+		assertEquals("Added: " + ITEM4 + ". \n", testInstance.determineAndExecuteCommand(ADD_COMMAND, ITEM4));
+	}
+	
+	
+	@Test
+	//Tests the Clear function.
+	public void test4() {
+		
+		assertEquals("All content cleared. \n", testInstance.determineAndExecuteCommand(CLEAR_COMMAND, null));
+		
+	}
+	
+	@Test
+	//Tests the Clear function's error message.
+	public void test5() {
+		
+		assertEquals("File is already empty. \n", testInstance.determineAndExecuteCommand(CLEAR_COMMAND, null));
+		
+	}
+	
+	@Test
+	//Tests the Display function's error message.
+	public void test6() {
+		
+		assertEquals("No content to display. \n", testInstance.determineAndExecuteCommand(DISPLAY_COMMAND, null));
+	}
+	
+	@Test
+	//Test the Delete function's error messages.
+	public void test7() {
+		
+		assertEquals("File is already empty. \n", testInstance.determineAndExecuteCommand(DELETE_COMMAND, "5"));
+		
+		assertEquals("Added: " + ITEM1 + ". \n", testInstance.determineAndExecuteCommand(ADD_COMMAND, ITEM1));
+		
+		assertEquals("Unable to remove non-existent line. Please input a number from: 1 to 1. \n", testInstance.determineAndExecuteCommand(DELETE_COMMAND, "5"));
+		
+		assertEquals("Added: " + ITEM2 + ". \n", testInstance.determineAndExecuteCommand(ADD_COMMAND, ITEM2));
+		assertEquals("Added: " + ITEM3 + ". \n", testInstance.determineAndExecuteCommand(ADD_COMMAND, ITEM3));
+		
+		assertEquals("Unable to remove non-existent line. Please input a number from: 1 to 3. \n", testInstance.determineAndExecuteCommand(DELETE_COMMAND, "5"));
+		
+		assertEquals("All content cleared. \n", testInstance.determineAndExecuteCommand(CLEAR_COMMAND, null));	
 	}
 		
 }
